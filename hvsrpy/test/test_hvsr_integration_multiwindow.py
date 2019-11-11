4174ec14fa69a7079825aec9346eb9197dca9c7a
Joseph vantassel <jvantassel@utexas.edu>
2019-11-11 17:13:51 -0600

    :white_check_mark: Add single- and multi-window integration tests

---
+++ hvsrpy/test/test_hvsr_integration_multiwindow.py
@@ -0,0 +1,64 @@
+"""This file contains an integration test for hvsrpy."""
+
+import numpy as np
+import hvsrpy as hv
+import pandas as pd
+import matplotlib.pyplot as plt
+
+timerecords = [
+    "test/data/a2/UT.STN11.A2_C50.miniseed",
+    "test/data/a2/UT.STN11.A2_C150.miniseed",
+    "test/data/a2/UT.STN12.A2_C50.miniseed",
+    "test/data/a2/UT.STN12.A2_C150.miniseed",
+]
+
+known_solutions = [
+    "test/data/integration/UT_STN11_c050.hv",
+    "test/data/integration/UT_STN11_c150.hv",
+    "test/data/integration/UT_STN12_c050.hv",
+    "test/data/integration/UT_STN12_c150.hv",
+]
+
+settings = {"length": 60,
+            "width": 0.1,
+            "b": 40,
+            "resampling": {"minf": 0.3,
+                           "maxf": 40,
+                           "nf": 2048, "res_type": "log"}
+            }
+bp_filter = {"flag": False, "flow": 0.001, "fhigh": 49.9, "order": 5}
+ratio_type = 'squared-average'
+distribution_type = 'log-normal'
+
+for fname, fname_geopsy in zip(timerecords, known_solutions):
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(4, 3))
+
+    sensor = hv.Sensor3c.from_mseed(fname)
+    my_hv = sensor.hv(settings["length"], bp_filter, settings["width"],
+                      settings["b"], settings["resampling"], ratio_type)    
+    for amp in my_hv.amp:
+        plt.plot(my_hv.frq, amp, color='#aaaaaa')
+    ax.plot(my_hv.frq, my_hv.mean_curve(), color='k', label="hvsrpy")
+    if distribution_type == "log-normal":
+        ax.plot(my_hv.frq, np.exp(np.log(my_hv.mean_curve()) +
+                                  my_hv.std_curve()), color='k', linestyle='--')
+        ax.plot(my_hv.frq, np.exp(np.log(my_hv.mean_curve()) -
+                                  my_hv.std_curve()), color='k', linestyle='--')
+    elif distribution_type == "normal":
+        ax.plot(my_hv.frq, my_hv.mean_curve(distribution_type) +
+                my_hv.std_curve(distribution_type), color='k', linestyle='--')
+        ax.plot(my_hv.frq, my_hv.mean_curve(distribution_type) -
+                my_hv.std_curve(distribution_type), color='k', linestyle='--')
+    else:
+        raise ValueError
+
+    geopsy_hv = pd.read_csv(fname_geopsy, delimiter="\t", comment="#", names=[
+                            "frq", "avg", "min", "max"])
+    ax.plot(geopsy_hv.frq, geopsy_hv["avg"], color='r', linestyle="--", label="Geopsy")
+    ax.plot(geopsy_hv.frq, geopsy_hv["min"], color='r', linestyle=':', label='')
+    ax.plot(geopsy_hv.frq, geopsy_hv["max"], color='r', linestyle=':', label='')
+    ax.set_xlabel("Frequency (Hz)")
+    ax.set_ylabel("H/V Ampltidue (#)")
+    ax.set_xscale('log')
+    ax.legend()
+    plt.savefig(f"../figs/multiwindow_{fname_geopsy[-13:-3]}.png", dpi=300, bbox_inches='tight')
---
+++ hvsrpy/test/test_hvsr_integration_singlewindow.py
@@ -0,0 +1,69 @@
+"""This file contains integration tests for a single window in hvsrpy."""
+
+import numpy as np
+import hvsrpy as hv
+import pandas as pd
+import matplotlib.pyplot as plt
+
+timerecords = [
+    "test/data/a2/UT.STN11.A2_C50.miniseed",
+    "test/data/a2/UT.STN11.A2_C50.miniseed",
+    "test/data/a2/UT.STN11.A2_C50.miniseed",
+    "test/data/a2/UT.STN11.A2_C50.miniseed",
+    "test/data/a2/UT.STN11.A2_C50.miniseed",
+    "test/data/a2/UT.STN11.A2_C50.miniseed",
+    "test/data/a2/UT.STN11.A2_C50.miniseed",
+    "test/data/a2/UT.STN11.A2_C50.miniseed",
+]
+
+known_solutions = [
+    "test/data/integration/UT_STN11_c50_single_a.hv",
+    "test/data/integration/UT_STN11_c50_single_b.hv",
+    "test/data/integration/UT_STN11_c50_single_c.hv",
+    "test/data/integration/UT_STN11_c50_single_d.hv",
+    "test/data/integration/UT_STN11_c50_single_e.hv",
+    "test/data/integration/UT_STN11_c50_single_f.hv",
+    "test/data/integration/UT_STN11_c50_single_g.hv",
+    "test/data/integration/UT_STN11_c50_single_h.hv",
+]
+
+settings = [
+    {"length": 60, "width": 0.1, "b": 40,
+     "resampling": {"minf": 0.3, "maxf": 40, "nf": 2048, "res_type": "log"}},
+    {"length": 120, "width": 0.1, "b": 40,
+     "resampling": {"minf": 0.3, "maxf": 40, "nf": 2048, "res_type": "log"}},
+    {"length": 60, "width": 0.1, "b": 10,
+     "resampling": {"minf": 0.3, "maxf": 40, "nf": 2048, "res_type": "log"}},
+    {"length": 60, "width": 0.1, "b": 80,
+     "resampling": {"minf": 0.3, "maxf": 40, "nf": 2048, "res_type": "log"}},
+    {"length": 60, "width": 0.2, "b": 40,
+     "resampling": {"minf": 0.3, "maxf": 40, "nf": 2048, "res_type": "log"}},
+    {"length": 60, "width": 0.02, "b": 40,
+     "resampling": {"minf": 0.3, "maxf": 40, "nf": 2048, "res_type": "log"}},
+    {"length": 60, "width": 0.1, "b": 40,
+     "resampling": {"minf": 0.3, "maxf": 40, "nf": 512, "res_type": "log"}},
+    {"length": 60, "width": 0.1, "b": 40,
+     "resampling": {"minf": 0.3, "maxf": 40, "nf": 4096, "res_type": "log"}},
+]
+
+bp_filter = {"flag": False, "flow": 0.001, "fhigh": 49.9, "order": 5}
+ratio_type = 'squared-average'
+distribution_type = 'log-normal'
+
+for setting, fname, fname_geopsy in zip(settings, timerecords, known_solutions):
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(4, 3))
+
+    sensor = hv.Sensor3c.from_mseed(fname)
+    my_hv = sensor.hv(setting["length"], bp_filter, setting["width"],
+                      setting["b"], setting["resampling"], ratio_type)
+    ax.plot(my_hv.frq, my_hv.amp[0], color='#aaaaaa', label="hvsrpy")
+
+    geopsy_hv = pd.read_csv(fname_geopsy, delimiter="\t", comment="#", names=[
+                            "frq", "avg", "min", "max"])
+    ax.plot(geopsy_hv.frq, geopsy_hv["avg"], color='r', linestyle="--", label="Geopsy")
+
+    ax.set_xlabel("Frequency (Hz)")
+    ax.set_ylabel("H/V Ampltidue (#)")
+    ax.set_xscale('log')
+    ax.legend()
+    plt.savefig(f"../figs/singlewindow_{fname_geopsy[-4]}.png", dpi=300, bbox_inches='tight')
