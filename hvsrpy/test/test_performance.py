0c3eb2b0dc1980eefa583595c3cbc9210372bfc7
Joseph vantassel <jvantassel@utexas.edu>
2019-11-12 09:43:23 -0600

    :racehorse: Update performance benchmark

--- hvsrpy/test/test_performance.py
+++ hvsrpy/test/test_performance.py
@@ -1,4 +1,4 @@
-"""This file contains a speed test for the main hvsr calculation."""
+"""This file contains a performance test for hvsr calculation."""
 
 import hvsrpy
 import cProfile
@@ -6,23 +6,24 @@ import pstats
 
 def main():
     windowlength = 60
-    flow = 0.1
-    fhigh = 45
-    forder = 5
-    width = 0.05
+    bp_filter = {"flag": True, "flow": 0.1, "fhigh": 45, "order": 5}    
+    width = 0.1
     bandwidth = 40
-    fmin = 0.1
-    fmax = 50
-    fn = 256
-    res_type = 'log'
-    ratio_type = 'geometric-mean'
+    resampling = {"minf": 0.1, "maxf": 50, "nf": 256, "res_type": "log"}
+    method = 'geometric-mean'
     n = 2
+    max_iter = 10
+
     sensor = hvsrpy.Sensor3c.from_mseed("test/data/a2/UT.STN11.A2_C50.miniseed")
-    hv = sensor.hv(windowlength, flow, fhigh, forder, width, bandwidth, fmin, fmax, fn, res_type, ratio_type)
-    hv.reject_windows(n, max_iterations=10)
+    hv = sensor.hv(windowlength, bp_filter, width, bandwidth, resampling, method)
+    hv.reject_windows(n, max_iter)
     
 fname = "test/.tmp_profiler_run"
 data = cProfile.run('main()', filename=fname)
 stat = pstats.Stats(fname)
 stat.sort_stats('tottime')
 stat.print_stats(0.01)
+
+# YEAR - MO - DY : TIME UNIT
+# -------------------------
+# 2019 - 11 - 12 : 1.242 s
