043255a48cdd5082e2d0dc92ca4435d3cd05fd6c
Joseph vantassel <jvantassel@utexas.edu>
2019-11-25 12:13:35 -0600

    :books: update version number in docs

--- doc/conf.py
+++ doc/conf.py
@@ -23,7 +23,7 @@ copyright = '2019, Joseph P. Vantassel'
 author = 'Joseph P. Vantassel'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
