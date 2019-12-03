670ab67a67e48fc9be77105e676db15be3517417
Joseph vantassel <jvantassel@utexas.edu>
2019-12-03 14:10:55 -0600

    :books: Update file docstring

--- hvsrpy/sensor3c.py
+++ hvsrpy/sensor3c.py
@@ -15,8 +15,7 @@
 #     You should have received a copy of the GNU General Public License
 #     along with this program.  If not, see <https: //www.gnu.org/licenses/>.
 
-"""This file contains a class for creating and manipulating 3-component
-sensor objects (Sensor3c)."""
+"""This file contains the 3-component sensor (Sensor3c) class."""
 
 import numpy as np
 from hvsrpy import Hvsr
