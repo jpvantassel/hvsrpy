28a6a8b1f66ffb67b61f18ea6d5b8782c4c6dd7a
Joseph vantassel <jvantassel@utexas.edu>
2020-03-19 23:24:17 -0500

    :racehorse: Update performance benchmarks

--- test/perf_rothvsr.py
+++ test/perf_rothvsr.py
@@ -15,7 +15,7 @@
 #     You should have received a copy of the GNU General Public License
 #     along with this program.  If not, see <https: //www.gnu.org/licenses/>.
 
-"""Performance test for single-station hvsr calculation."""
+"""Performance test for rotated hvsr calculation."""
 
 import hvsrpy
 import cProfile
@@ -24,20 +24,24 @@ from testtools import get_full_path
 
 full_path = get_full_path(__file__)
 
+
 def main():
     windowlength = 60
-    bp_filter = {"flag": True, "flow": 0.1, "fhigh": 45, "order": 5}    
+    bp_filter = {"flag": True, "flow": 0.1, "fhigh": 45, "order": 5}
     width = 0.1
     bandwidth = 40
     resampling = {"minf": 0.1, "maxf": 50, "nf": 256, "res_type": "log"}
-    method = 'geometric-mean'
+    method = 'rotate'
+    azimuth = [0, 15, 30, 45, 60, 75, 90, 105, 120, 135, 150, 165]
     n = 2
-    max_iter = 10
+    max_iter = 50
 
-    sensor = hvsrpy.Sensor3c.from_mseed(full_path+"data/a2/UT.STN11.A2_C50.miniseed")
-    hv = sensor.hv(windowlength, bp_filter, width, bandwidth, resampling, method)
-    hv.reject_windows(n, max_iter)
-    
+    fname = full_path+"data/a2/UT.STN11.A2_C50.miniseed" 
+    sensor = hvsrpy.Sensor3c.from_mseed(fname)
+    hv = sensor.hv(windowlength, bp_filter, width,
+                   bandwidth, resampling, method, azimuth=azimuth)
+    hv.reject_windows(n=n, max_iterations=max_iter)
+
 fname = full_path+"data/.tmp_profiler_run"
 data = cProfile.run('main()', filename=fname)
 stat = pstats.Stats(fname)
@@ -46,6 +50,4 @@ stat.print_stats(0.01)
 
 # YEAR - MO - DY : TIME UNIT
 # -------------------------
-# 2019 - 11 - 12 : 1.242 s
-# 2020 - 03 - 19 : 1.300 s -> After overhaul of sigpropy smoothing.
-# 2020 - 03 - 19 : 0.313 s -> Add caching to sigpropy.
+# 2020 - 03 - 19 : 1.092 s -> Add caching to sigpropy.
