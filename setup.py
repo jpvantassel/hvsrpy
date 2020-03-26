20aada627ab0ddd57eaa063181388b2c21bd4bd2
Joseph vantassel <jvantassel@utexas.edu>
2020-03-26 15:47:37 -0500

    :art: A few minor changes

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", encoding="utf8") as f:
 
 setup(
     name='hvsrpy',
-    version='0.2.2',
+    version='0.3.0',
     description='A Python package for horizontal-to-vertical spectral ratio processing',
     long_description=long_description,
     long_description_content_type='text/markdown',
