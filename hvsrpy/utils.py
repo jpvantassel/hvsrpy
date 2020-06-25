8c65a7cd10e859267a051aded6806fa9bfb32d79
Joseph vantassel <jvantassel@utexas.edu>
2020-06-25 17:24:31 -0500

    :sparkles: Initial working version of sesame clarity check

--- hvsrpy/utils.py
+++ hvsrpy/utils.py
@@ -20,35 +20,105 @@
 from hvsrpy import Hvsr
 import numpy as np
 
-def sesame_clarity(mean_curve, std_curve):
+
+def sesame_clarity(frequency, mean_curve, std_curve, f0_std):
     """Check SESAME (2004) clarity criteria.
-    
+
     Parameters
     ----------
+    frequency : ndarray
+        Frequency vector for H/V curve.
     mean_curve : ndarray
-        Mean H/V curve. Note mean must be from assuming a lognormal
-        distribution.
+        Mean H/V curve
+        (assumes lognormal distribution).
     std_curve : ndarray
-        Standard deviation of H/V curve. Note standard deviation must be
-        from assuming a lognormal distribution.
-    
+        Standard deviation of H/V curve
+        (assumes lognormal distribution).
+    f0_std : float
+        Standard deviation of f0 peak
+        (assumes normal distribution).
 
+    Returns
+    -------
+    ndarray
+        Of length 6 (one per condition), indicating a pass with a `1`
+        and a fail with a `0`.
 
     """
+
+    def peak_index(curve):
+        pot_peak_indices, _ = Hvsr.find_peaks(curve)
+        pot_peak_amp = curve[pot_peak_indices]
+        pot_peak_index = np.argwhere(pot_peak_amp == np.max(pot_peak_amp))[0][0]
+        peak_index = pot_peak_indices[pot_peak_index]
+        return peak_index
+
     criteria = np.zeros(6)
 
-    # Find peaks
-    peaks_indices, _ = Hvsr.find_peaks(mean_curve)
+    # Peak Information
+    mc_peak_index = peak_index(mean_curve)
+    mc_peak_frq = frequency[mc_peak_index]
+    mc_peak_amp = mean_curve[mc_peak_index]
 
-    # Find peak with highest amplitude
-    potential_peaks = mean_curve[peaks_indices]
-    peak_index = np.where(potential_peaks == np.max(potential_peaks))[0]
+    # Criteria i)
+    a_low = mean_curve[np.logical_and(frequency < mc_peak_frq,
+                                      frequency > mc_peak_frq/4)]
+    if np.sum(a_low < mc_peak_amp/2):
+        criteria[0] = 1
+
+    # Criteria ii)
+    a_high = mean_curve[np.logical_and(frequency > mc_peak_frq,
+                                       frequency < 4*mc_peak_frq)]
     
-    # Peak
-    mc_peak_amp = potential_peaks[peak_index]
+    if np.sum(a_high < mc_peak_amp/2):
+        criteria[1] = 1
 
-    # Criteria iii) A0>2
+    # Criteria iii)
     if mc_peak_amp > 2:
+        criteria[2] = 1
+
+    # Criteria iv)
+    upper_curve = np.exp(np.log(mean_curve) + std_curve)
+    lower_curve = np.exp(np.log(mean_curve) - std_curve)
+
+    upper_peak_index = peak_index(upper_curve)
+    lower_peak_index = peak_index(lower_curve)
+
+    f_plus = frequency[upper_peak_index]
+    f_minus = frequency[lower_peak_index]
+
+    cond_1 = f_plus > mc_peak_frq*0.95 and f_plus < mc_peak_frq*1.05
+    cond_2 = f_minus > mc_peak_frq*0.95 and f_minus < mc_peak_frq*1.05
+
+    if cond_1 and cond_2:
         criteria[3] = 1
-    
-    raise NotImplementedError
+
+    # Table for conditions v) and vi)
+    if mc_peak_frq < 0.2:
+        epsilon = 0.25
+        theta = 3
+    elif mc_peak_frq < 0.5:
+        epsilon = 0.2
+        theta = 2.5
+    elif mc_peak_frq < 1:
+        epsilon = 0.15
+        theta = 2
+    elif mc_peak_frq < 2:
+        epsilon = 0.1
+        theta = 1.78
+    else:
+        epsilon = 0.05
+        theta = 1.58
+
+    # Criteria v)
+    if f0_std < epsilon*mc_peak_frq:
+        criteria[4] = 1
+
+    # Criteria vi)
+    sigma_a = upper_curve/mean_curve
+    sigma_a_peak = sigma_a[mc_peak_index]
+
+    if sigma_a_peak < theta:
+        criteria[5] = 1
+
+    return criteria
--- test/test_utils.py
+++ test/test_utils.py
@@ -17,24 +17,139 @@
 
 """Tests for utils module."""
 
-from testtools import TestCase, unittest, get_full_path
 import pandas as pd
+import numpy as np
 
 from hvsrpy import utils
+from testtools import TestCase, unittest, get_full_path
+
 
 class Test_Utils(TestCase):
-    
+
     @classmethod
     def setUpClass(cls):
         cls.full_path = get_full_path(__file__)
 
-    def test_sesame(self):
-        df = pd.read_csv(self.full_path + "data/utils/ex0.csv")
-        mean_curve = df["mean"].to_numpy()
-        std_curve = df["p1"].to_numpy()
+    def test_sesame_by_condition(self):
+        # Defaults
+        frequency = np.array([0.7, 0.8, 0.9, 1, 1.1])
+        mean_curve = np.array([1, 1, 2.1, 1, 1])
+        std_curve = np.array([0.2, 0.2, 0.2, 0.2, 0.2])
+        f0_std = 0.1
 
-        self.assertRaises(NotImplementedError, utils.sesame_clarity, mean_curve, std_curve)
+        # Condition 1: there exists an f in [f0/4, f0] where A(f) < A(f0)/2
+        frequency = np.array([0.2, 0.4, 0.7, 1, 1.5])
+        potential_curves = [(1, np.array([1.0, 1.0, 1.0, 3.0, 1.0])),
+                            (0, np.array([2.9, 2.9, 2.9, 3.0, 1.0])),
+                            (0, np.array([1.6, 2.0, 2.9, 3.0, 1.0])),
+                            (1, np.array([1.4, 1.4, 2.0, 3.0, 1.0])),
+                            (0, np.array([1.5, 1.5, 2.9, 3.0, 1.0]))
+                            ]
+
+        for expected, mean_curve in potential_curves:
+            returned = utils.sesame_clarity(frequency, mean_curve,
+                                            std_curve, f0_std)[0]
+            self.assertEqual(expected, returned)
+
+        # Condition 2: there exists an f in [f0, 4f0] where A(f) < A(f0)/2
+        frequency = np.array([0.7, 1, 2.5, 3.5, 5])
+        potential_curves = [(1, np.array([1.0, 3.0, 1.0, 1.0, 1.0])),
+                            (0, np.array([1.0, 3.0, 2.9, 2.9, 2.9])),
+                            (0, np.array([1.0, 3.0, 2.9, 2.0, 1.6])),
+                            (1, np.array([1.0, 3.0, 2.0, 1.4, 1.4])),
+                            (0, np.array([1.0, 3.0, 2.9, 1.5, 1.5]))
+                            ]
+
+        for expected, mean_curve in potential_curves:
+            returned = utils.sesame_clarity(frequency, mean_curve,
+                                            std_curve, f0_std)[1]
+            self.assertEqual(expected, returned)
+
+        # Condition 3: A0>2
+        potential_curves = [(0, np.array([1.0, 1.0, 1.1, 1.0, 1.0])),
+                            (0, np.array([1.0, 2.0, 1.0, 1.0, 1.0])),
+                            (0, np.array([1.0, 1.0, 1.0, 1.2, 1.0])),
+                            (1, np.array([1.0, 3.0, 2.0, 1.4, 1.4])),
+                            (1, np.array([1.0, 5.0, 2.9, 1.5, 1.5])),
+                            (1, np.array([1.0, 3.0, 2.9, 7.5, 1.5]))
+                            ]
+        for expected, mean_curve in potential_curves:
+            returned = utils.sesame_clarity(frequency, mean_curve,
+                                            std_curve, f0_std)[2]
+            self.assertEqual(expected, returned)
+
+        # Condition 4: fpeak[Ahv(f) +/- sigma_a(f)] = f0 +/- 5%
+        frequency = np.array([0.90, 0.94, 0.96, 1, 1.04, 1.06, 1.1])
+
+        mean_curves = [np.array([1.0, 1.0, 1.0, 2.0, 1.0, 1.0, 1.0]),
+                       np.array([1.0, 1.0, 1.0, 2.0, 1.0, 1.0, 1.0]),
+                       np.array([1.0, 1.0, 2.0, 1.0, 1.0, 1.0, 1.0]),
+                       np.array([1.0, 1.0, 1.0, 1.0, 2.0, 1.0, 1.0]),
+                       np.array([1.0, 1.0, 1.0, 2.0, 1.0, 1.0, 1.0]),
+                       np.array([1.0, 1.0, 1.0, 2.0, 1.0, 1.0, 1.0]),
+                       np.array([1.0, 1.0, 1.0, 2.0, 1.0, 1.0, 1.0])]
+
+        uppers = [np.array([1.0, 1.1, 1.0, 1.0, 1.0, 1.0, 1.0])+0.1,
+                  np.array([1.0, 1.0, 1.0, 1.0, 1.0, 1.1, 1.0])+0.1,
+                  np.array([1.0, 1.0, 1.0, 1.0, 1.0, 1.1, 1.0])+0.1,
+                  np.array([1.0, 1.1, 1.0, 1.0, 1.0, 1.0, 1.0])+0.1,
+                  np.array([1.0, 1.0, 1.0, 2.1, 1.0, 1.0, 1.0])+0.1,
+                  np.array([1.0, 1.0, 1.1, 1.0, 1.0, 1.0, 1.0])+0.1,
+                  np.array([1.0, 1.0, 1.0, 1.0, 1.1, 1.0, 1.0])+0.1]
+
+        rating = [0, 0, 0, 0, 1, 1, 1]
+
+        for expected, upper, mean_curve in zip(rating, uppers, mean_curves):
+            std_curve = np.log(upper) - np.log(mean_curve)
+            curve = np.exp(np.log(mean_curve) + std_curve)
+            self.assertArrayEqual(upper, curve)
+            returned = utils.sesame_clarity(frequency, mean_curve,
+                                            std_curve, f0_std)[3]
+            self.assertEqual(expected, returned)
+
+        # Condition 5: f0_std < epsilon(f0)
+        frequency = np.array([0.05, 0.1, 0.35, 0.75, 1.5, 3, 5])
+        factors = [0.25, 0.2, 0.15, 0.1, 0.05]
+
+        for peak_frequency, factor in zip(frequency[1:-1], factors):
+            mean_curve = np.ones_like(frequency)
+            mean_curve[peak_frequency == frequency] = 2
+            std_curve = 0.1*mean_curve
+            for adjust, expected in zip([0.9, 1.1], [1, 0]):
+                f0_std = factor*peak_frequency*adjust
+                returned = utils.sesame_clarity(frequency, mean_curve,
+                                                std_curve, f0_std)[4]
+                self.assertEqual(expected, returned)
+
+        # Condition 6: sigma_a(f0) < theta(f0)
+        frequency = np.array([0.05, 0.1, 0.35, 0.75, 1.5, 3, 5])
+        factors = [3.0, 2.5, 2.0, 1.78, 1.58]
+
+        for peak_frequency, factor in zip(frequency[1:-1], factors):
+            mean_curve = np.ones_like(frequency)
+            mean_curve[peak_frequency == frequency] = 2
+            for adjust, expected in zip([0.9, 1.1], [1, 0]):
+                sigma_a = np.ones_like(frequency)*adjust*factor
+                upper = mean_curve * sigma_a
+                std_curve = np.log(upper) - np.log(mean_curve)
+                curve = np.exp(np.log(mean_curve) + std_curve)
+                self.assertArrayAlmostEqual(upper, curve, places=6)
+                f0_std = factor*peak_frequency*adjust
+                returned = utils.sesame_clarity(frequency, mean_curve,
+                                                std_curve, f0_std)[5]
+                self.assertEqual(expected, returned)
+
+    def test_sesame_by_case(self):
+        # Case 1: All pass
+        frequency = np.array([0.7, 0.8, 0.9, 1, 1.1])
+        mean_curve = np.array([1, 1, 2.1, 1, 1])
+        std_curve = np.array([0.2, 0.2, 0.2, 0.2, 0.2])
+        f0_std = 0.1
+
+        returned = utils.sesame_clarity(frequency, mean_curve, std_curve,
+                                        f0_std)
+        expected = np.array([1.0]*6)
+        self.assertArrayEqual(expected, returned)
 
 if __name__ == "__main__":
     unittest.main()
-
