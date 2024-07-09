eafead5224401cb2151b4f85210c37b8675d4857
Joseph Vantassel <joseph.p.vantassel@gmail.com>
2024-07-09 09:50:15 -0400

    :wrench: Up version to 2.0.0 and list support for Python 3.11 and 3.12

--- setup.py
+++ setup.py
@@ -64,6 +64,8 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
     keywords='horizontal-to-vertical spectral ratio hv hvsr',
     packages=find_packages(),
